338fd890259c0772989967c8bbb93f95ee509638
AntPerDev <perezterreroantonio@gmail.com>
2020-12-17 16:35:35 +0100

    corregido urls.py

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -4,4 +4,5 @@ from .views import polls_list, polls_detail
 
 urlpatterns = [
     path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+    path("polls/<int:pk>/", polls_detail, name="polls_detail"),
+]
