83435bbc098834a3bc3d5b066cda737f0fce489d
AntPerDev <perezterreroantonio@gmail.com>
2020-12-18 12:04:59 +0100

    Fine grained access control

--- pollsapi/polls/apiviews.py
+++ pollsapi/polls/apiviews.py
@@ -23,11 +23,14 @@
 from rest_framework import generics
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Poll, Choice
 from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer, UserSerializer
+
+from django.contrib.auth import authenticate
 
 
 class PollList(generics.ListCreateAPIView):
@@ -50,6 +53,12 @@ class ChoiceList(generics.ListCreateAPIView):
 
     serializer_class = ChoiceSerializer
 
+    def post(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not create choice for this poll.")
+        return super().post(request, *args, **kwargs)
+
 
 # class CreateVote(generics.CreateAPIView):
 # serializer_class = VoteSerializer
@@ -70,8 +79,27 @@ class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not delete this poll.")
+        return super().destroy(request, *args, **kwargs)
+
 
 class UserCreate(generics.CreateAPIView):
     authentication_classes = ()
     permission_classes = ()
     serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request,):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
